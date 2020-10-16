d019f0584c4c1d66b705c325e85b746d7a2e72cd
Sarah Peters <sarahpieters15@gmail.com>
2020-10-16 13:34:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,54 +1,52 @@
 class Song
   attr_accessor :name, :artist, :genre
+
   @@count = 0
   @@genres = []
   @@artists = []
 
+  def initialize(song_name, artist, genre)
+    @name = song_name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
 
-def initialize(song_name, artist, genre)
-  @@count += 1
-  @@genres << genre
-  @@artists << artist
-  @genre = genre
-  @name = song_name
-  @artist = artist
-end
+  def self.count
+    @@count
+  end
 
-end
+  def self.genres
+    @@genres.uniq!
+  end
 
-def self.count
-  @@count
-end
+  def self.artists
+    @@artists.uniq!
+  end
 
-def self.genres
-  @@genres.uniq!
-end
-
-def self.artists
-  @@artists.uniq!
-end
-
-def self.genre_count
+  def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-      if genre_count[genre]
+      if genre_count[genre] #if the genre is already in the hash
         genre_count[genre] += 1
-      else
+      else #if it's a new genre
         genre_count[genre] = 1
       end
     end
     genre_count
   end
 
-def self.artist_count
-  artist_count = {}
-  @@artists.each do |artist|
-    if artist_count[artist]
-      artist_count[artist] += 1
-    else
-      artist_count[artist] = 1
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist] #if the artist is already in the hash
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
     end
+    artist_count
   end
-  artist_count
-end
 end
