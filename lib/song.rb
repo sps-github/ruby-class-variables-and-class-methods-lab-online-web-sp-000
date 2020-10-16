e7cb4d5d74f4571131815e6285349f70aeeb2954
Sarah Peters <sarahpieters15@gmail.com>
2020-10-16 13:28:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,10 +28,27 @@ def song.artists
   @@artists.uniq!
 end
 
-def song.genre_count
-  @@genre_count
-end
+ef self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
 
 def song.artist_count
-  
+  artist_count = {}
+  @@artists.each do |artist|
+    if artist_count[artist]
+      artist_count[artist] += 1
+    else
+      artist_count[artist] = 1
+    end
+  end
+  artist_count
 end
+  
