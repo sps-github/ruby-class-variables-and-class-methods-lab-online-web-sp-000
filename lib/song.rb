4410c0b616894f9dd856c28d6555c5e9e0d8991a
Sarah Peters <sarahpieters15@gmail.com>
2020-10-16 13:18:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,20 @@
 class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
+  @@genres = []
+
 
 def initialize(name, artist, genre)
   @@count += 1
+  
 end
 
 end
 
 def song.count
   @@count
+end
+
+def song.genres
+  @@genres
 end
